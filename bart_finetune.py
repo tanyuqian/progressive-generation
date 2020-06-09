b9461d3d5f02ad3dcae391800585f35232b700f5
bwt09 <tanbowen@sjtu.edu.cn>
2020-06-09 11:37:04 -0400

    updated.

--- bart_finetune.py
+++ bart_finetune.py
@@ -17,16 +17,16 @@ def get_vocab(dataset, vocab_size):
     return pickle.load(open(f'data/{dataset}/vocab_{vocab_size}.pickle', 'rb'))
 
 
-def load_data(dataset, split, vocab_size, keep_condition):
-    if vocab_size in ['null', 'full']:
+def load_data(dataset, split, vocab, keep_condition):
+    if vocab in ['null', 'full']:
         examples = pickle.load(open(f'data/{dataset}/{split}.pickle', 'rb'))
-        if vocab_size == 'null':
+        if vocab == 'null':
             return [example['condition'] for example in examples]
         else:
             return [example['text'] for example in examples]
     else:
         examples = pickle.load(open(
-            f'data/{dataset}/extracted_{split}_{vocab_size}words.pickle', 'rb'))
+            f'data/{dataset}/extracted_{split}_{vocab}words.pickle', 'rb'))
         if keep_condition:
             return [example['condition'] + ' [SEP] ' + example['extracted_text']
                     for example in examples]
@@ -34,21 +34,19 @@ def load_data(dataset, split, vocab_size, keep_condition):
             return [example['extracted_text'] for example in examples]
 
 
-def main(vocab_size_src='null',
-         vocab_size_tgt='full',
-         dataset='writing_prompts',
+def main(dataset='writing_prompts',
+         src_vocab='null',
+         tgt_vocab='full',
          n_epochs=3):
 
-    if os.path.exists(f'bart_{vocab_size_src}-{vocab_size_tgt}_training_logs'):
+    if os.path.exists(f'bart_{dataset}_{src_vocab}-{tgt_vocab}_training_logs'):
         return
 
     bart = BART()
 
     for split in ['train', 'dev']:
-        src_texts = load_data(
-            dataset, split, vocab_size_src, keep_condition=True)
-        tgt_texts = load_data(
-            dataset, split, vocab_size_tgt, keep_condition=False)
+        src_texts = load_data(dataset, split, src_vocab, keep_condition=True)
+        tgt_texts = load_data(dataset, split, tgt_vocab, keep_condition=False)
 
         bart.load_data(set_type=split, src_texts=src_texts, tgt_texts=tgt_texts)
 
@@ -63,9 +61,9 @@ def main(vocab_size_src='null',
 
     bart.create_training_log(
         eval_steps=len(bart.dataset['train']) // BATCH_SIZE,
-        label=f'bart_{dataset}_{vocab_size_src}-{vocab_size_tgt}')
+        label=f'bart_{dataset}_{src_vocab}-{tgt_vocab}')
 
-    noise_vocab = get_vocab(dataset, vocab_size_src)
+    noise_vocab = get_vocab(dataset, src_vocab)
     for epoch in range(n_epochs):
         bart.train_epoch(batch_size=BATCH_SIZE, noise_vocab=noise_vocab)
 
