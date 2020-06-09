e2b0dc6185b1322805f548e5843375d7b92533c1
bwt09 <tanbowen@sjtu.edu.cn>
2020-06-09 15:23:09 -0400

    updated.

--- train.py
+++ train.py
@@ -7,14 +7,25 @@ def main(dataset, prog_steps, first_model):
     assert prog_vocabs[0] == 'null' and prog_vocabs[-1] == 'full'
 
     for vocab in prog_steps[1:-1]:
-        os.system(f'python make_stage.py --dataset {dataset} --rate {vocab}')
+        os.system(f'python make_stage.py '
+                  f'--dataset {dataset} '
+                  f'--rate {vocab}')
 
     if first_model == 'bart':
-        os.system(f'python bart_finetune.py --dataset {dataset} '
-                  f'--src_vocab null --tgt_vocab {prog_vocabs[1]}')
+        os.system(f'python bart_finetune.py '
+                  f'--dataset {dataset} '
+                  f'--src_vocab null '
+                  f'--tgt_vocab {prog_vocabs[1]}')
     else:
-        os.system(f'python gpt2_finetune.py --dataset {dataset} '
-                  f'--vocab {prog_vocabs[1]} --gpt2_type {first_model}')
+        os.system(f'python gpt2_finetune.py '
+                  f'--dataset {dataset} '
+                  f'--vocab {prog_vocabs[1]} '
+                  f'--gpt2_type {first_model}')
+
+    for i in range(1, len(prog_steps)):
+        os.system(f'python bart_finetune.py --dataset {dataset} '
+                  f'--src_vocab {prog_vocabs[i]} '
+                  f'--tgt_vocab {prog_vocabs[i+1]}')
 
 
 if __name__ == '__main__':
